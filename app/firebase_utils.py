57d4e7dc2139e67767137b6796446c3546037207
Alfredo Santos <thirdsantos666@gmail.com>
2025-06-03 16:57:42 +0800

    Commit this code

---
+++ app/firebase_utils.py
@@ -0,0 +1,39 @@
+import firebase_admin
+from firebase_admin import credentials, db
+import os
+import json
+from datetime import datetime
+
+
+firebase_key_json = os.environ.get('FIREBASE_KEY')
+
+
+firebase_key = json.loads(firebase_key_json)
+
+# Initialize Firebase
+if not firebase_admin._apps:
+    cred = credentials.Certificate(firebase_key)
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://plate-number-detection-a7574-default-rtdb.asia-southeast1.firebasedatabase.app/'
+    })
+
+def upload_plate_data(data):
+    try:
+        latest_id = db.reference('latest_id').get()
+        if latest_id is None:
+            latest_id = 0
+        else:
+            latest_id += 1
+
+        db.reference('latest_id').set(latest_id)
+
+        ref = db.reference(f'detected_plates/{latest_id}')
+        ref.set({
+            'plate_number': data.get('Plate Number', 'Not detected'),
+            'vehicle_type': data.get('Vehicle Type', 'Unknown'),
+            'timestamp': data.get('Timestamp', datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        })
+        return True
+    except Exception as e:
+        print(f"Error uploading to Firebase: {str(e)}")
+        return False
---
+++ app/routes.py
@@ -0,0 +1,47 @@
+from flask import Blueprint, request, jsonify, render_template
+import requests
+from .firebase_utils import upload_plate_data
+
+bp = Blueprint('routes', __name__)
+
+API_TOKEN = 'd81dc1b204aea00b9e846861cee62bb8dd2e2c0b'
+@bp.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return jsonify({'error': 'No file part'}), 400
+        
+        file = request.files['image']
+        regions = ['sg', 'ph']
+
+        response = requests.post(
+            'https://api.platerecognizer.com/v1/plate-reader/',
+            data={'regions': regions},
+            files={'upload': file},
+            headers={'Authorization': f'Token {API_TOKEN}'}
+        )
+
+        if response:
+            result = response.json()
+
+            if result['results']:
+                top_result = result['results'][0]
+                plate_number = top_result.get('plate')
+                vehicle_type = top_result.get('vehicle', {}).get('type', 'Unknown')
+
+            timestamp = result.get('timestamp', 'N/A')
+            
+            data_result = {
+                "Plate Number": plate_number,
+                "Vehicle Type": vehicle_type,
+                "Timestamp": timestamp
+            }
+
+            upload_plate_data(data_result)
+            
+            return jsonify(data_result), 200
+        
+        return jsonify({'error': 'Failed to process image'}), 400
